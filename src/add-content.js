4cbf540eb40c771645c90a584c931f20c73b380a
andy.ten <andy.ten@tom.com>
2021-04-17 15:43:23 +0800

    【提交类型】：optimization 【提交描述】：测试代码优化

--- src/add-content.js
+++ src/add-content.js
@@ -4,9 +4,9 @@
  * @Author: andy.ten@tom.com
  * @Date: 2021-04-17 13:39:17
  * @LastEditors: andy.ten@tom.com
- * @LastEditTime: 2021-04-17 13:40:01
+ * @LastEditTime: 2021-04-17 15:38:37
  * @Version: 1.0.1
  */
 export default function () {
-  document.write('hello world');
+  document.write('hello world using webpack-dev-server live-reloading ! <br/>');
 }
