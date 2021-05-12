c6277d90f2b10be1df9c891fd51fcaaeb38abf87
andy.ten <andy.ten@tom.com>
2021-05-12 15:43:54 +0800

    【提交类型】：optimization 【提交描述】：新增图片处理demo

---
+++ src/vendor/add-fileloader.js
@@ -0,0 +1,20 @@
+/*
+ * @Copyright: Copyright (c) 2021 andyten
+ * @Description:
+ * @Author: andy.ten@tom.com
+ * @Date: 2021-05-10 14:45:10
+ * @LastEditors: andy.ten@tom.com
+ * @LastEditTime: 2021-05-10 18:04:40
+ * @Version: 1.0.1
+ */
+
+// 绝对路径
+import avatarImage from '../assets/img/avatar.jpg';
+
+console.log(avatarImage);
+
+const img01 = document.getElementById('img01');
+img01.width = '100';
+img01.height = '100';
+img01.src = avatarImage;
+
