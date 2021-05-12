35bf24e9c8ed47ecf159c8fcb212590e770bec95
andy.ten <andy.ten@tom.com>
2021-05-12 15:44:36 +0800

    【提交类型】：optimization 【提交描述】：demo样例优化

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@
  * @Author: andy.ten@tom.com
  * @Date: 2021-04-17 13:36:36
  * @LastEditors: andy.ten@tom.com
- * @LastEditTime: 2021-04-26 21:33:45
+ * @LastEditTime: 2021-05-11 20:37:18
  * @Version: 1.0.1
  */
-import addContent from './vendor/add-content';
-import path from 'path';
-import './assets/css/style.css';
+import _addContent from './vendor/add-content';
+import _path from 'path';
+import './assets/css/style.scss';
+import './vendor/add-fileloader';
 
 // const path = require('path');
 document.write('My first Webpack app.<br />');
-addContent();
-document.write('dirname path:' + __dirname + ' <br/>');
-document.write('dirname.join:' + path.join(__dirname, '..', 'src/index.js') + ' <br/>');
-document.write('dirname.resolve:' + path.resolve(__dirname, '/dist') + ' <br/>');
+_addContent();
+document.write('dirname path:' + _path.join(__dirname) + ' <br/>');
+document.write('dirname.join:' + _path.join(__dirname, '..', 'src/index.js') + ' <br/>');
+document.write('dirname.resolve:' + _path.resolve(__dirname, '/dist') + ' <br/>');
--- src/vendor/add-content.js
+++ src/vendor/add-content.js
@@ -4,7 +4,7 @@
  * @Author: andy.ten@tom.com
  * @Date: 2021-04-17 13:39:17
  * @LastEditors: andy.ten@tom.com
- * @LastEditTime: 2021-04-25 13:59:50
+ * @LastEditTime: 2021-04-28 16:39:24
  * @Version: 1.0.1
  */
 
@@ -12,7 +12,7 @@ import _path from 'path';
 
 export default function() {
   document.write('hello world using webpack-dev-server live-reloading ! <br/>');
-  document.write('dirname2:' + __dirname + ' <br/>');
+  document.write('dirname2:' + _path.join(__dirname) + ' <br/>');
   document.write('dirname.join2:' + _path.join(__dirname, '..', 'src/index.js') + ' <br/>');
   document.write('dirname.resolve2:' + _path.resolve(__dirname, '/dist') + ' <br/>');
 }
