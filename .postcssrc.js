f03b428562f9ead7cf0bc87efe25ede1fc1cfa33
andy.ten <andy.ten@tom.com>
2021-05-30 13:33:07 +0800

    【提交类型】：optimization 【提交描述】：增加postcss自动前缀配置

---
+++ .postcssrc.js
@@ -0,0 +1,9 @@
+/**
+ * postcss增加自动前缀，使用前提是在package.json中定义browserslist
+ */
+module.exports = {
+  plugins: {
+    // to edit target browsers: use "browserslist" field in package.json
+    autoprefixer: {}
+  }
+};
